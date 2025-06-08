4cfc243d9d5346a0aa68fa2db0dcb82ac230331a
Nikhilkadam49 <nikhilkadam4901@gmail.com>
2025-06-09 00:22:48 +0530

    v3

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ module "vpc" {
 
 module "subnet" {
     source = "./subnet"
-    vpc_id    = module.vpc.vpc_id
+    #vpc_id    = module.vpc.vpc_id
 }
 
 module "ec2" {
     source = "./ec2"
-    vpc_id    = module.vpc.vpc_id
+    #vpc_id    = module.vpc.vpc_id
 }
 
 
